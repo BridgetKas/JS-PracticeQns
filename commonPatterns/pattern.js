72065ba9ebeef0512720aa0f5e223fa60dd19daf
BridgetKas <bridgetnamugga@gmail.com>
2024-11-20 09:47:29 +0300

    Added a pattern question

--- commonPatterns/pattern.js
+++ commonPatterns/pattern.js
@@ -10,4 +10,42 @@ function printRectangle(rows,columns) {
     }
 }
 
-printRectangle(5,5)
+// printRectangle(5,5)
+
+// Write a js program that prints a triangle
+
+function printTraingle(columns) {
+    for(let i = 0; i<columns; i++) {
+        let triangle = ""
+        for(let j = 0; j<=i; j++) {
+            triangle += "*"
+        }
+        console.log(triangle)
+    }
+}
+
+// printTraingle(6)
+
+function printTraingle(columns) {
+    for(let i = 1; i<=columns; i++) {
+        let triangle = ""
+        for(let j = 1; j<=i; j++) {
+            triangle += j
+        }
+        console.log(triangle)
+    }
+}
+
+// printTraingle(6)
+
+function printInvertedTraingle(columns) {
+    for(let i = 1; i<=columns; i++) {
+        let triangle = ""
+        for(let j = 1; j<=columns-i+1; j++) {
+            triangle += "*"
+        }
+        console.log(triangle)
+    }
+}
+
+printInvertedTraingle(6)
